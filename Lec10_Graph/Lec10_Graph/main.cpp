3546977f70046eb1361860faf1890198f083f2d7
dbswodyd1 <sk48197228@gmail.com>
2026-05-29 10:19:00 +0900

    가중치그래프

--- Lec10_Graph/Lec10_Graph/main.cpp
+++ Lec10_Graph/Lec10_Graph/main.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "AdjMatGraph.h"	// 그래프 클래스 포함
-
 int main()
 {
     SrchAMGraph g;	// 새로운 그래프 객체 생성
@@ -37,6 +36,15 @@ int main()
     WGraph wg;
     wg.load("../graph.txt");    //graph 텍스트 파일 읽어오기
     wg.display();               //display
+
+    WGraphMST g;
+    g.load("graph.txt");
+    // printf("입력 그래프: graph.txt\n");
+    // g.display("graph.txt");
+    printf("MST By Kruskal’s Algorithm\n");
+    g.Kruskal();
+
+
 
     return 0;
 }
